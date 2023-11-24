d31a839a3e4878c6e41d306380d176442104a856
Ryotaro-Seriu <ryotaro.seriu.5s@stu.hosei.ac.jp>
2023-11-24 18:00:58 +0900

    wordテーブルから出題。点数表示機能追加。

--- resources/views/typings/index.blade.php
+++ resources/views/typings/index.blade.php
@@ -1,23 +1,18 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Typing</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    </head>
-    <body class="antialiased">
-    <x-app-layout>
-        <x-slot name="header">
-            <h1>詳細画面</h1>
-            <h2>{{ Auth::user()->name }} さんこんにちは</h2>
-        </x-slot>
-        <h2 class='practice'>
-            <a href="/typing/practice">練習する</a>
-        </h2>
+<x-app-layout>
+    <x-slot name="header">
+        <h1>詳細画面</h1>
+        <h2>{{ Auth::user()->name }} さんこんにちは</h2>
+    </x-slot>
+    <h2 class='practice'>
+        <a href="/typing/practice">練習する</a>
+    </h2>
+    <script>
+        'use strict'
         
-    </x-app-layout>
-    
-    </body>
-</html>
+        const words = @json($words);
+        console.log(words);
+        
+        const fruits = words.map((word)=>word.english_word);
+        console.log(fruits);
+    </script>
+</x-app-layout>
